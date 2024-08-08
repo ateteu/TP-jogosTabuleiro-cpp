9cfabb6eae8413cb2b950f98ed4d84bae3029bba
IanGodoi <116098512+IanGodoi@users.noreply.github.com>
2024-08-08 16:36:04 -0300

    Add files via upload

---
+++ src/Jogador.hpp
@@ -0,0 +1,34 @@
+//
+//  Jogador.hpp
+//  tp
+//
+//  Created by Ian Godoi on 06/08/24.
+//
+
+#ifndef Jogador_hpp
+#define Jogador_hpp
+
+#include <iostream>
+#include <string>
+class Jogador{
+private:
+    std::string nome_jogador;
+    int ponto;
+    int id;
+    
+public:
+    Jogador();
+    Jogador( const std::string& nome_jogador= "", int ponto = 0);
+    ~Jogador();
+ 
+
+    std::string get_nome() const;
+    void set_nome(const std:: string& nome);
+    
+    int get_pontos() const;
+    void set_pontos(int pontos);
+    
+    void imprimir_jogador() const;
+};
+
+#endif /* Jogador_hpp */
