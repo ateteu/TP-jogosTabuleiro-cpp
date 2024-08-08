cf6b685d3a7f2e22c7c9e8c0b7d360f13ad61e89
IanGodoi <116098512+IanGodoi@users.noreply.github.com>
2024-08-08 16:50:51 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Jogador.hpp"
-#include "Gerenciamento_de_jogadores.hpp"
+#include "Cadastro_de_jogadores.hpp"
 int main(int argc, const char * argv[]) {
     
     return 0;
