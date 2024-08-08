99f3b7cbd7aa08a292e939d99fe8639639368b30
IanGodoi <116098512+IanGodoi@users.noreply.github.com>
2024-08-08 16:35:25 -0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tp
+//
+//  Created by Ian Godoi on 06/08/24.
+//
+
+#include <iostream>
+#include <string>
+#include "Jogador.hpp"
+#include "Gerenciamento_de_jogadores.hpp"
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
