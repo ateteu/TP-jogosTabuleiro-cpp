01b5273a5c0c98520740565aee205e4db8d34dea
IanGodoi <116098512+IanGodoi@users.noreply.github.com>
2024-08-08 18:14:53 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 #include "Cadastro_de_jogadores.hpp"
 int main(int argc, const char * argv[]) {
     
+    //Cadastro_de_jogadores  cadastro;
+    //Jogador jogador("Lucas");
+    //cadastro.adicionar_jogador_no_arquivo(jogador);
+    //cadastro.remover_jogador_do_arquivo("Lucas");
+    //cadastro.buscar_jogador_no_arquivo("Rafael");
+   // cadastro.atualizar_nome_do_jogador("João", "Maria");
+   // cadastro.listar_jogadores_do_arquivo();
+
+   
     return 0;
 }
 
