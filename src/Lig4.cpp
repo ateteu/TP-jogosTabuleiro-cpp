2b48565c348badd638153bc41b46e8e63a3fc482
Paulo Vitor da Silva Rodrigues <paulovrodrigues@hotmail.com>
2024-08-17 18:44:11 -0300

    lig4: atributos

---
+++ src/Lig4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "Jogo.hpp"
+
+class Lig4 : public Jogo {
+private:
+    static const int largura = 7; // Largura do tabuleiro
+    static const int altura = 6;  // Altura do tabuleiro
+    std::vector<std::vector<char>> tabuleiro;
+
+
+}
