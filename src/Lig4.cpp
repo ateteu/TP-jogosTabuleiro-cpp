9171a59c82064fe84a89f8bca840fc88f9ab0d57
Paulo Vitor da Silva Rodrigues <paulovrodrigues@hotmail.com>
2024-08-17 18:47:50 -0300

    lig4: cabeçalho

--- src/Lig4.cpp
+++ src/Lig4.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
-#include <vector>
-#include "Jogo.hpp"
+#ifndef LIG4_H
+#define LIG4_H
+
+#include "Jogo.h"
 
 class Lig4 : public Jogo {
+public:
+    // Construtor
+    Lig4();
+
+    // Sobrescrições dos métodos virtuais da classe base Jogo
+    void inicializarTabuleiro() override;
+    bool validarJogada(int coluna) override;
+    bool verificarCondicaoVitoria() override;
+    void imprimirTabuleiro() override;
+    void realizarJogada(int coluna) override;
+
 private:
-    static const int largura = 7; // Largura do tabuleiro
-    static const int altura = 6;  // Altura do tabuleiro
-    std::vector<std::vector<char>> tabuleiro;
+    static const int largura = 7;  // Largura do tabuleiro para Lig4
+    static const int altura = 6;   // Altura do tabuleiro para Lig4
+};
 
-
-}
+#endif // LIG4_H
