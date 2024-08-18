9bfadc2668b2649fa07a51a3c223d4b5a82dc971
João Marcos <joaomarcos.5566@gmail.com>
2024-08-18 18:59:00 -0300

    manutecao nas classes Jogo, Tabuleiro e lig4

--- include/Lig4.hpp
+++ include/Lig4.hpp
@@ -2,6 +2,7 @@
 #define LIG4_HPP
 
 #include "Jogo.hpp"
+#include "Tabuleiro.hpp"
 
 class Lig4 : public Jogo {
 public:
@@ -10,13 +11,14 @@ public:
 
     // Sobrescrições dos métodos virtuais da classe base Jogo
     void inicializarTabuleiro() override;
-    bool validarJogada(int linha, int coluna, Jogador jogador) override; 
-    bool verificarCondicaoVitoria() override;
+    bool validarJogada(int linha, int coluna, Jogador* jogador) override; 
+    int verificarCondicaoVitoria() override;
     void realizarJogada(int coluna) override;
 
 private:
     static const int largura = 7;  // Largura do tabuleiro para Lig4
     static const int altura = 6;   // Altura do tabuleiro para Lig4
+    Tabuleiro* tabuleiro = new Tabuleiro();
 };
 
 #endif // LIG4_HPP
