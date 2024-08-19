2760bbc80d3edb1755ba04c560b042601e764ff9
João Marcos <joaomarcos.5566@gmail.com>
2024-08-19 20:02:39 -0300

    Adiciona arquivo .gitignore

---
+++ build/CMakeFiles/TP-jogosTabuleiro-cpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/CadastroDeJogadores.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/CadastroDeJogadores.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/CadastroDeJogadores.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/Jogador.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogador.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogador.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/Jogo.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogo.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogo.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/Lig4.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Lig4.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Lig4.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/Reversi.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Reversi.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Reversi.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/SistemaDeJogo.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/SistemaDeJogo.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/SistemaDeJogo.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/Tabuleiro.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Tabuleiro.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Tabuleiro.cpp.o.d"
+  "/home/joao-linux/TP-jogosTabuleiro-cpp/src/main.cpp" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TP-jogosTabuleiro-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/CadastroDeJogadores.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/CadastroDeJogadores.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogador.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogador.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogo.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Jogo.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Lig4.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Lig4.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Reversi.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Reversi.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/SistemaDeJogo.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/SistemaDeJogo.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Tabuleiro.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/Tabuleiro.cpp.o.d"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/main.cpp.o"
+  "CMakeFiles/TP-jogosTabuleiro-cpp.dir/src/main.cpp.o.d"
+  "TP-jogosTabuleiro-cpp"
+  "TP-jogosTabuleiro-cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP-jogosTabuleiro-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
