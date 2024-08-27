8e6092c5a147f67ee79097ce6d9b1a2cd00f4adb
Igor Henrique <igorhenrique0472@gmail.com>
2024-08-27 19:28:18 -0300

    correções de bugs em Lig4

--- include/Lig4.hpp
+++ include/Lig4.hpp
@@ -3,6 +3,8 @@
 
 #include "Jogo.hpp"
 #include "Tabuleiro.hpp"
+#include "Jogador.hpp"
+#include "SistemaDeJogo.hpp"
 
 /**
  * @class Lig4
@@ -13,6 +15,7 @@
  * condições de vitória e a realização das jogadas.
  */
 class Lig4 : public Jogo {
+
 public:
     /**
      * @brief Construtor da classe Lig4.
@@ -20,7 +23,7 @@ public:
      * Inicializa o tabuleiro com as dimensões específicas de Lig4 e define
      * as peças dos jogadores.
      */
-    Lig4();
+    Lig4(Jogador* _jogador1, Jogador* _jogador2);
 
     /**
      * @brief Inicializa o tabuleiro do jogo Lig4.
@@ -61,9 +64,12 @@ public:
 private:
     static const int largura = 7;  /**< Largura do tabuleiro para Lig4. */
     static const int altura = 6;   /**< Altura do tabuleiro para Lig4. */
-    Tabuleiro* tabuleiro = new Tabuleiro(); /**< Ponteiro para o tabuleiro do jogo Lig4. */
+    Tabuleiro tabuleiro; /**< Ponteiro para o tabuleiro do jogo Lig4. */
     char jogador1Peca = 'X'; /**< Peça do jogador 1. */
     char jogador2Peca = 'O'; /**< Peça do jogador 2. */
+    Jogador* jogador1; /**< Ponteiro para o jogador 1. */
+    Jogador* jogador2; /**< Ponteiro para o jogador 2. */
+    bool vezJogador1; /**< Indica se é a vez do jogador 1. */
 };
 
 #endif // LIG4_HPP
