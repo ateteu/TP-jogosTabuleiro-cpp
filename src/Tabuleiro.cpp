8656fe4e295d7e26dd073fb8f41866b59f480944
Igor Henrique <igorhenrique0472@gmail.com>
2024-08-26 21:30:55 -0300

    atualizações em Tabuleiro

--- src/Tabuleiro.cpp
+++ src/Tabuleiro.cpp
@@ -1,44 +1,56 @@
-class Tabuleiro {
-private:
-    int linhas;
-    int colunas;
-    char** matrizTabuleiro;
+#include "../include/Tabuleiro.hpp"
+#include <iostream>
 
-public:
-    // Construtor
-    Tabuleiro() : linhas(0), colunas(0), matrizTabuleiro(nullptr) {}
+Tabuleiro::Tabuleiro() : linhas(0), colunas(0) {}
 
-    // Destrutor
-    ~Tabuleiro() {
-        liberarMemoria();
+void Tabuleiro::configurarTabuleiro(int _linhas, int _colunas) {
+    // Verifica se há uma matriz alocada previamente e libera a memória
+    if (matrizTabuleiro) {
+        matrizTabuleiro.reset();
     }
 
-    void configurarTabuleiro(int _linhas, int _colunas) {
-        // Libera a memória da matrizTabuleiro, se já alocada
-        liberarMemoria();
+    linhas = _linhas;
+    colunas = _colunas;
 
-        // Atualiza as dimensões
-        linhas = _linhas;
-        colunas = _colunas;
-
-        // Aloca matriz para o tabuleiro
-        matrizTabuleiro = new char*[linhas];
-        for (int i = 0; i < linhas; i++) {
-            matrizTabuleiro[i] = new char[colunas];
-            for (int j = 0; j < colunas; j++) {
-                matrizTabuleiro[i][j] = '.'; // inicializa as posições com ponto
-            }
+    // Aloca a matriz para o tabuleiro usando smart pointers
+    matrizTabuleiro = std::make_unique<std::unique_ptr<char[]>[]>(linhas);
+    for (int i = 0; i < linhas; i++) {
+        matrizTabuleiro[i] = std::make_unique<char[]>(colunas);
+        for (int j = 0; j < colunas; j++) {
+            matrizTabuleiro[i][j] = '.'; // Inicializa as posições com ponto
         }
     }
 
-private:
-    void liberarMemoria() {
-        if (matrizTabuleiro) {
-            for (int i = 0; i < linhas; i++) {
-                delete[] matrizTabuleiro[i];
-            }
-            delete[] matrizTabuleiro;
-            matrizTabuleiro = nullptr;
-        }
+    // Imprime o tabuleiro após a configuração
+    imprimir();
+}
+
+void Tabuleiro::definirPosicao(int _x, int _y, char _peca) {
+    if (posicaoValida(_x, _y)) {
+        matrizTabuleiro[_x][_y] = _peca;
+    } else {
+        throw std::out_of_range("Posição inválida no tabuleiro");
     }
-};
+}
+
+char Tabuleiro::obterPeca(int _x, int _y) const {
+    if (posicaoValida(_x, _y)) {
+        return matrizTabuleiro[_x][_y];
+    }
+    return '.'; // Retorna um '.' se a posição for inválida
+}
+
+bool Tabuleiro::posicaoValida(int _x, int _y) const {
+    return (_x >= 0 && _x < linhas && _y >= 0 && _y < colunas);
+}
+
+void Tabuleiro::imprimir() const {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            std::cout << matrizTabuleiro[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+Tabuleiro::~Tabuleiro() = default; // Destrutor padrão
